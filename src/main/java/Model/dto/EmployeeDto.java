6511e99400b80b1ecd7bbc7db712625b4ae62375
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-12 19:39:53 +0530

    Changed EmployeeDto for hibernate

--- src/main/java/Model/dto/EmployeeDto.java
+++ src/main/java/Model/dto/EmployeeDto.java
@@ -1,5 +1,9 @@
 package Model.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Getter
@@ -7,15 +11,26 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Entity
+@Table(name = "employee")
 public class EmployeeDto {
+    @Id
+    @Column(name = "employee_id")
     private String employeeId;
+
     private String name;
     private String nic;
     private String dob;
     private String position;
     private double salary;
+
+    @Column(name = "ContactNumber")
     private String ContactNumber;
+
     private String address;
+
+    @Column(name = "joinDate")
     private String joinedDate;
+
     private String status;
 }
